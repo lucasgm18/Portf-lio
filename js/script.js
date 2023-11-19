e2bba65d4b05bb5f63cbc0f3cc62c2774f9d6503
Lukita18 <lucasgomesm1808@gmail.com>
2023-11-18 23:55:06 -0300

    adicionando função de clique no menu

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick = () => {
+   menuIcon.classList.toggle('bx-x');
+}
+
 
 
 let sections = document.querySelectorAll('section');
