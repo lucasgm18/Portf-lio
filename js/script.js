640762953a026579d74eae52f45a532cca3f98cb
Lukita18 <lucasgomesm1808@gmail.com>
2023-11-19 01:18:38 -0300

    corrigindo erros no document query, e add the menu icon and classlist remove

--- js/script.js
+++ js/script.js
@@ -23,13 +23,16 @@ window.onscroll = () => {
     if(top >= offset && top < offset + height) {
        navLinks.forEach(links => {
         links.classList.remove('active');
-        document.querySelector('header nav a[href*=]' + id + ']').classList.add('active');
-       })
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+       });
     }
    });
 
     let header = document.querySelector('header')
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
 
